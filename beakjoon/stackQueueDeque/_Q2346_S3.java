bb30d49e9f0c5ca253c78b001ef1489c994ba89a
jy016011 <39334789+jy016011@users.noreply.github.com>
2023-10-16 20:41:27 +0900

    Update _Q2346_S3.java

--- beakjoon/stackQueueDeque/_Q2346_S3.java
+++ beakjoon/stackQueueDeque/_Q2346_S3.java
@@ -3,8 +3,8 @@ package beakjoon.stackQueueDeque;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class _Q2346_S3 {
@@ -12,7 +12,7 @@ public class _Q2346_S3 {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(bufferedReader.readLine());
         StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        Deque<int[]> balloons = new LinkedList<>();
+        Deque<int[]> balloons = new ArrayDeque<>();//LinkedList will cause memory exceeded
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = 1; i <= N; i++) {
             int[] tmp = {i, Integer.parseInt(stringTokenizer.nextToken())};
