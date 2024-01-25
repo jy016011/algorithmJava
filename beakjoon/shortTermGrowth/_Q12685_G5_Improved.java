4cc62ef559ac083cc7b73326290d02f4bf353f7c
jy016011 <39334789+jy016011@users.noreply.github.com>
2024-01-25 23:58:45 +0900

    [BOJ] 2024/01/25 solved, add short term growth package
    백준 단기간 성장 문제집 패키지 추가
    각 문제를 풀고,
    설명을 기입할 예정
    
    Q12865(DP solved)
    1. use matrix dp
    2. use array dp(improved)

---
+++ beakjoon/shortTermGrowth/_Q12685_G5_Improved.java
@@ -0,0 +1,30 @@
+package beakjoon.shortTermGrowth;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _Q12685_G5_Improved {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int N = Integer.parseInt(stringTokenizer.nextToken());
+        int[] weights = new int[N + 1];
+        int[] values = new int[N + 1];
+        int K = Integer.parseInt(stringTokenizer.nextToken()); // max weight
+        int[] dp = new int[K + 1];
+        for (int i = 1; i <= N; i++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            weights[i] = Integer.parseInt(stringTokenizer.nextToken());
+            values[i] = Integer.parseInt(stringTokenizer.nextToken());
+        }
+
+        for (int i = 1; i <= N; i++) {
+            for (int k = K; k - weights[i] >= 0; k--) {
+                dp[k] = Math.max(dp[k], dp[k - weights[i]] + values[i]);
+            }
+        }
+        System.out.println(dp[K]);
+    }
+}
---
+++ beakjoon/shortTermGrowth/_Q12865_G5.java
@@ -0,0 +1,38 @@
+package beakjoon.shortTermGrowth;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _Q12865_G5 {
+    private static int N, K;
+    private static int result = Integer.MIN_VALUE;
+    private static int[] weights, values;
+    private static int[][] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        N = Integer.parseInt(stringTokenizer.nextToken());
+        weights = new int[N + 1];
+        values = new int[N + 1];
+        K = Integer.parseInt(stringTokenizer.nextToken()); // max weight
+        dp = new int[K + 1][N + 1]; // index is available weight
+        for (int i = 1; i <= N; i++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            weights[i] = Integer.parseInt(stringTokenizer.nextToken());
+            values[i] = Integer.parseInt(stringTokenizer.nextToken());
+        }
+        for (int k = 1; k <= K; k++) {
+            for (int i = 1; i <= N; i++) {
+                if (weights[i] > k) {
+                    dp[k][i] = dp[k][i - 1];
+                    continue;
+                }
+                dp[k][i] = Math.max(dp[k - weights[i]][i - 1] + values[i], dp[k][i - 1]);
+            }
+        }
+        System.out.println(dp[K][N]);
+    }
+}
