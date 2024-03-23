887e6d241ca67826d81a33ca8eb8eb20ccc24673
jy016011 <39334789+jy016011@users.noreply.github.com>
2024-03-23 09:29:50 +0900

    240323 solved
    resolved

---
+++ beakjoon/dynamicProgramming/gold/_Q12865_G5.java
@@ -0,0 +1,29 @@
+package beakjoon.dynamicProgramming.gold;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _Q12865_G5 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int N = Integer.parseInt(stringTokenizer.nextToken());
+        int[] weights = new int[N];
+        int[] values = new int[N];
+        int K = Integer.parseInt(stringTokenizer.nextToken());
+        int[] dp = new int[K + 1];
+        for (int i = 0; i < N; i++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            weights[i] = Integer.parseInt(stringTokenizer.nextToken());
+            values[i] = Integer.parseInt(stringTokenizer.nextToken());
+        }
+        for (int i = 0; i < N; i++) {
+            for (int k = K; k >= weights[i]; k--) {
+                dp[k] = Math.max(dp[k], dp[k - weights[i]] + values[i]);
+            }
+        }
+        System.out.println(dp[K]);
+    }
+}
