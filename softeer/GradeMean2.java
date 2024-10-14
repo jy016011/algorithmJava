d7ab1809846280dfa5bf330083ca3fa07be61e2f
jy016011 <39334789+jy016011@users.noreply.github.com>
2024-10-15 03:08:00 +0900

    test commit

--- softeer/GradeMean2.java
+++ softeer/GradeMean2.java
@@ -1,2 +1,30 @@
-package softeer;public class GradeMean2 {
+package softeer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class GradeMean2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int N = Integer.parseInt(stringTokenizer.nextToken());
+        int K = Integer.parseInt(stringTokenizer.nextToken());
+        int[] S = new int[N + 1];
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        for (int i = 1; i <= N; i++) {
+            S[i] = S[i - 1] + Integer.parseInt(stringTokenizer.nextToken());
+        }
+        StringBuilder results = new StringBuilder();
+        for (int i = 0; i < K; i++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int start = Integer.parseInt(stringTokenizer.nextToken());
+            int end = Integer.parseInt(stringTokenizer.nextToken());
+            int gradeSum = S[end] - S[start - 1];
+            int numberOfStudents = end - start + 1;
+            results.append(String.format("%.2f", (double) gradeSum / numberOfStudents)).append(System.lineSeparator());
+        }
+        System.out.println(results);
+    }
 }
